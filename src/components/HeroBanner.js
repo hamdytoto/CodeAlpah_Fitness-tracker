65005cff83fe6cd49307627a7965a737364b7ba7
hamdytoto <hamdytoto>
2024-05-28 17:50:53 +0300

    the last touch

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,58 +1,61 @@
 import React from "react";
-import { Box, Stack, Typography, Button } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+
 import HeroBannerImage from "../assets/images/banner.jpg";
 
-const HeroBanner = () => {
-	return (
-		<Box
-			sx={{ mt: { lg: "40px", xs: "70px" }, ml: { sm: "50px" } }}
-			position="relative"
-			p="20px"
+const HeroBanner = () => (
+	<Box
+		sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }}
+		position="relative"
+		p="20px"
+	>
+		<Typography color="#FF2625" fontWeight="600" fontSize="26px">
+			The Gym
+		</Typography>
+		<Typography
+			fontWeight={700}
+			sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+			mb="23px"
+			mt="30px"
 		>
-			<Typography
+			Sweat, Smile <br />
+			And Repeat
+		</Typography>
+		<Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
+			Check out the most effective exercises personalized to you
+		</Typography>
+		<Stack>
+			<a
+				href="#exercises"
 				style={{
-					color: "red",
-					fontSize: "30px",
-					fontFamily: "fantasy",
-					fontWeight: "600",
-				}}
-			>
-				The Gym
-			</Typography>
-			<Typography
-            mb="23px"
-            mt="25px"
-				style={{
-					fontWeight: "700",
-				}}
-				sx={{ fontSize: { lg: "44px", xs: "40px" } }}
-			>
-				Cursh Your Health <br /> And Fitness
-			</Typography>
-			<Typography
-				style={{
+					marginTop: "45px",
+					textDecoration: "none",
+					width: "200px",
+					textAlign: "center",
+					background: "#FF2625",
+					padding: "14px",
 					fontSize: "22px",
-					lineHeight: "35px",
+					textTransform: "none",
+					color: "white",
+					borderRadius: "4px",
 				}}
-				mb={4}
-                
 			>
-				Check out the most effective exercises
-			</Typography>
-			<Button variant="contained" color="error" href="#exercises" sx={{backgroundColor:"#ed1c24",padding:"10px"}}>
 				Explore Exercises
-			</Button>
-			<Typography
-				fontSize="200px"
-				fontWeight={600}
-				color="#ff2625"
-				sx={{ opacity: 0.1, display: { lg: "block", xs: "none" } }}
-			>
-				Exercises
-			</Typography>
-			<img  src={HeroBannerImage} alt="banner" className="hero-banner-img" />
-		</Box>
-	);
-};
+			</a>
+		</Stack>
+		<Typography
+			fontWeight={600}
+			color="#FF2625"
+			sx={{
+				opacity: "0.1",
+				display: { lg: "block", xs: "none" },
+				fontSize: "200px",
+			}}
+		>
+			Exercise
+		</Typography>
+		<img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
+	</Box>
+);
 
 export default HeroBanner;
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,83 +1,64 @@
-import React, { useState, useEffect } from "react";
-import { Button, Stack, TextField, Box, Typography } from "@mui/material";
-import { exerciseOptions, fetchData } from "../utils/fetchData";
-import HorizontalScrollbar from "./HorizontalScrollbar"; 
-const SearchExercises = (setExercises,bodyPart,setBodyPart) => {
-	const [search, setSearch] = useState("");
-	const [bodyParts, setBodyParts] = useState([]);
-	useEffect(() => {
-		const fetchExercisesData = async () => {
-			const bodyPartsData = await fetchData(
-				'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
-				exerciseOptions
-			);
-			setBodyParts(["all", ...bodyPartsData]);
-		};
-		fetchExercisesData();
-	}, []);
-	const handleSearch = async () => {
-		if (search) {
-			const exercisesData = await fetchData(
-				"https://exercisedb.p.rapidapi.com/exercises",
-				exerciseOptions
-			);
-			// console.log(exerciesData);
-			const searchedExercises = exercisesData.filter(
-				(exercise) => exercise.name.toLowerCase().includes(search) 
-                ||exercise.target.toLowerCase().includes(search) 
-                ||exercise.equipment.toLowerCase().includes(search)
-                ||exercise.bodyPart.toLowerCase().includes(search)
-			);
-			setSearch('');
-			setExercises(searchedExercises);
-		}
-	};
-	return (
-		<Stack alignItems="center" justifyContent="center" mt="37px" padding="20px">
-			<Typography
-				mb="50px"
-				textAlign="center"
-				fontWeight={700}
-				sx={{ fontSize: { lg: "44px", xs: "30px" } }}
-			>
-				Awesome exerciese you <br /> should know
-			</Typography>
-			<Box position="relative" mb="72px">
-				<TextField
-					sx={{
-						input: { fontWeight: "700", border: "none", borderRadius: "4px" },
-						width: { lg: "800px", xs: "350px" },
-						backgroundColor: "#fff",
-						borderRadius: "40px",
-					}}
-					type="text"
-					height="76px"
-					value={search}
-					onChange={(e) => setSearch(e.target.value.toLowerCase())}
-					placeholder="search Exercieses"
-				/>
-				<Button
-					onClick={handleSearch}
-					className="search-btn"
-					sx={{
-						bgcolor: " #ed1c24",
-						color: "black",
-						textTransform: "none",
-						width: { lg: "174px", xs: "80px" },
-						fontSize: { lg: "20px", xs: "14px" },
-						height: "56px",
-						position: "absolute",
-						right: "0",
-					}}
-				>
-					Search
-				</Button>
-			</Box>
-			<Box sx={{ position: "relative", width: "100%", padding: "20px" }}>
-				<HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
-			</Box>
-		</Stack>
-	);
-}; 
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
+
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
+  const [search, setSearch] = useState('');
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+      setBodyParts(['all', ...bodyPartsData]);
+    };
+
+    fetchExercisesData();
+  }, []);
+
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+
+      const searchedExercises = exercisesData.filter(
+        (item) => item.name.toLowerCase().includes(search)
+               || item.target.toLowerCase().includes(search)
+               || item.equipment.toLowerCase().includes(search)
+               || item.bodyPart.toLowerCase().includes(search),
+      );
+
+      window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
+
+      setSearch('');
+      setExercises(searchedExercises);
+    }
+  };
+
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
+        Awesome Exercises You <br /> Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          height="76px"
+          sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }}
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search Exercises"
+          type="text"
+        />
+        <Button className="search-btn" sx={{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch}>
+          Search
+        </Button>
+      </Box>
+      <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
+        <HorizontalScrollbar data={bodyParts} bodyParts setBodyPart={setBodyPart} bodyPart={bodyPart} />
+      </Box>
+    </Stack>
+  );
+};
 
 export default SearchExercises;
